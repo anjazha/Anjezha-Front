2a4f7eb7d86a6304ca73bc98573050c82c72d93e
tahashabaan <tahashabaan48@gmail.com>
2024-09-28 08:19:02 +0300

    solve issue in become tasker

--- src/functions/axiosInstance.ts
+++ src/functions/axiosInstance.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 
+const  baseURL1="http://localhost:5000/api/v1"
+const  baseURL="https://anjezha-production.up.railway.app/api/v1"
+
 export const axiosInstance = axios.create({
     baseURL:"https://e-learning-0wji.onrender.com/api/v1",
+    // baseURL,
     withCredentials:true,
 })
